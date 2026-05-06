ca7bc9b0120da11fef338c5ef225e7991cb76f41
Oskarhn <oskarhn06@outlook.com>
2026-05-06 09:11:25 +0200

    Slave2 Kode

--- DatamaskinArkitektur/Prosjekt/Slave2/adc.h
+++ DatamaskinArkitektur/Prosjekt/Slave2/adc.h
@@ -1,16 +1,19 @@
 /*
- * adc.h - ADC-bibliotek for master
+ * adc.h - ADC-bibliotek for slave 2
  * ING1507 Datamaskinarkitektur
  * ATmega32 @ 1MHz
  *
  * Sensor: LM35 DFR0023 pa PA0 (ADC0)
  * Referanse: AVCC (5V)
- * Prescaler: 8 -> ADC-klokke = 125kHz (gyldig: 50-200kHz)
+ * Prescaler: 8 -> ADC-klokke = 125kHz
  *
  * LM35 formel:
  *   Vout = 10mV per grad Celsius
- *   ADC = (Vout / VREF) * 1024 = (Temp * 0.01 / 5.0) * 1024
- *   Temp = (ADC * 500) / 1024
+ *   Temp(degC) = (ADC * 500) / 1024
+ *
+ * Eksempel: 25 grader -> Vout = 250mV
+ *   ADC = (0.25 / 5.0) * 1024 = 51
+ *   Temp = (51 * 500) / 1024 = 24.9 ~ 25 grader
  */
  
 #ifndef ADC_H
@@ -21,38 +24,32 @@
  
 void ADC_Init(void)
 {
-    /* REFS1:0 = 01 -> AVCC som referanse */
-    /* ADLAR   = 0  -> hoyre-justert      */
-    /* MUX4:0  = 00000 -> ADC0 (PA0)      */
-    ADMUX = (1 << REFS0);
+    /* REFS1:0 = 01 -> AVCC referanse */
+    /* MUX4:0  = 00000 -> ADC0 (PA0) */
+    ADMUX  = (1 << REFS0);
  
-    /* ADEN = 1 aktiver ADC                  */
-    /* ADPS2:0 = 011 -> prescaler 8 (125kHz) */
+    /* ADEN=1, prescaler 8 */
     ADCSRA = (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0);
 }
  
-uint16_t ADC_Read(uint8_t channel)
+static uint16_t ADC_Read_Raw(void)
 {
-    /* Velg kanal, behold referanseinnstilling */
-    ADMUX = (ADMUX & 0xE0) | (channel & 0x1F);
+    /* Velg ADC0 (kanal 0) */
+    ADMUX &= 0xE0;
  
-    ADCSRA |= (1 << ADSC);              /* Start konvertering */
-    while (ADCSRA & (1 << ADSC));       /* Vent pa ferdig     */
- 
-    return ADC;  /* Les 10-bit resultat (ADCL+ADCH) */
+    ADCSRA |= (1 << ADSC);         /* Start */
+    while (ADCSRA & (1 << ADSC));  /* Vent  */
+    return ADC;
 }
  
 /*
- * Leser temperatur fra LM35 pa ADC0.
- * Returnerer temperatur i hele grader Celsius.
+ * Leser LM35-temperatur.
+ * Returnerer temperatur i hele grader Celsius (int16_t).
  */
 int16_t LM35_Read_Temp(void)
 {
-    uint16_t adc_val = ADC_Read(0); /* PA0 = ADC0 */
-    /* Temp = (ADC * VREF * 100) / 1024
-     *      = (ADC * 5000mV) / 1024 / 10mV
-     *      = (ADC * 500) / 1024                */
-    return (int16_t)((uint32_t)adc_val * 500UL / 1024UL);
+    uint16_t raw = ADC_Read_Raw();
+    return (int16_t)((uint32_t)raw * 500UL / 1024UL);
 }
  
 #endif /* ADC_H */
